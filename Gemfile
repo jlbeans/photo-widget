4b9cf67fa97a576742839502412de43b580b4391
jlbeans <jm@cselmer.com>
2022-03-29 14:58:24 -0700

    Add views for static_pages

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-gem 'flickr', '~> 2.0', '>= 2.0.2'
+gem 'flickr'
 gem 'figaro'
 gem 'pusher'
 gem 'simple_form'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,17 @@
 class StaticPagesController < ApplicationController
+
+def index
+  begin
+    require 'flickr'
+    flickr = Flickr.new
+    if params[:user_id].present?
+      @photos = flickr.photos.search(user_id: params[:user_id])
+    else
+      @photos = flickr.photos.getRecent
+    end
+  rescue StandardError => e
+    flash[:alert] = "#{e.class}: #{e.message}. Something went wrong.."
+    redirect_to root_path
+  end
+ end
 end
